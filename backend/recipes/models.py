3d931925b80d8bfe4388f48f2423b311885ce6d1
«Сергей <sergei.eliseev93@yandex.ru>
2023-03-13 00:05:49 +0300

    3

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,4 +1,3 @@
-from colorfield.fields import ColorField
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models import UniqueConstraint
