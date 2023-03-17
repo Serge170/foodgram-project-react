18eb6e747c4b5af31ceec808c5d7df4f22e07f91
«Сергей <sergei.eliseev93@yandex.ru>
2023-03-17 11:05:49 +0300

    ver_7

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,20 +1,34 @@
 '''Настройка админ панели'''
 
 from django.contrib import admin
+from users.models import Subscriptions, User
 
-from .models import User
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     '''Модель пользователя для админки'''
+#     list_display = (
+#         'pk',
+#         'username',
+#         'first_name',
+#         'last_name',
+#         'email',
+#     )
+#     list_filter = ('username', 'email')
+#     search_fields = ('username', 'email')
+#     empty_value_display = '-пусто-'
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    '''Модель пользователя для админки'''
-    list_display = (
-        'pk',
-        'username',
-        'first_name',
-        'last_name',
-        'email',
-    )
-    list_filter = ('username', 'email')
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
     search_fields = ('username', 'email')
+    list_filter = ('username', 'email')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Subscriptions)
+class SubscriptionsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'author')
+    search_fields = ('user', 'author')
+    list_filter = ('user', 'author')
     empty_value_display = '-пусто-'
