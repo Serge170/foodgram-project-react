93c05aa08c3d86b1840257c9cff6f77c7deff37c
«Сергей <sergei.eliseev93@yandex.ru>
2023-03-21 13:04:20 +0300

    ver_8

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -31,7 +31,7 @@ class RecipesFilter(FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(favorites__user=self.request.user)
+            return queryset.filter(favorite_recipes__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
