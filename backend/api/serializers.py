7700c02c01cb0ac0ac885470b0aa57ca7c0c3a9c
«Сергей <sergei.eliseev93@yandex.ru>
2023-03-25 01:08:59 +0300

    final

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -225,24 +225,40 @@ class RecipesCreateSerializer(serializers.ModelSerializer):
             'text', 'cooking_time'
         )
 
-    def validate_ingredients(self, value):
-        if not value:
-            raise ValidationError({
-                'ingredients': 'Нужен хотя бы один ингредиент!'
-            })
-        ingredients_list = []
-        for item in value:
-            ingredient = get_object_or_404(Ingredients, id=item['id'])
-            if ingredient in ingredients_list:
-                raise ValidationError({
-                    'ingredients': 'Ингридиенты не должны повторяться!'
-                })
-            if int(item['amount']) <= 0:
-                raise ValidationError({
-                    'amount': 'Количество ингредиента должно быть больше 0!'
-                })
-            ingredients_list.append(ingredient)
-        return value
+    def validate(self, data):
+        ingredients = self.initial_data.get('ingredients')
+        tags = self.initial_data.get('tags')
+        if not ingredients:
+            raise serializers.ValidationError(
+                'Необходим минимум один ингредиент для рецепта'
+            )
+        if not tags:
+            raise serializers.ValidationError(
+                'Необходим минимум один тег для рецепта'
+            )
+        ingredient_list = []
+        for ingredient_item in ingredients:
+            if not Ingredients.objects.filter(
+                    pk=ingredient_item['id']).exists():
+                raise serializers.ValidationError(
+                    'Ингредиента с таким id не существует'
+                )
+            if int(ingredient_item['id']) in ingredient_list:
+                raise serializers.ValidationError('Укажите уникальный '
+                                                  'ингредиент')
+            if 'amount' not in ingredient_item:
+                raise serializers.ValidationError('Количество ингредиента '
+                                                  'не может быть пустым')
+            amount = int(ingredient_item['amount'])
+            if amount <= 0 or amount > 32766:
+                raise serializers.ValidationError(
+                    'Укажите корректное количество ингредиента, '
+                    'в диапазоне от 0,01 до 32766'
+                )
+            ingredient_list.append(ingredient_item['id'])
+        data['ingredients'] = ingredients
+        data['tags'] = tags
+        return data
 
     @staticmethod
     def create_ingredients(ingredients, recipes):
